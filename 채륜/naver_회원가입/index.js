e18553e6e2d964e0286f1536d5fd56a1994ab2df
PBEM22 <dlacofbs1229@gmail.com>
2023-05-15 10:29:03 +0900

    중간 저장

--- 채륜/naver_회원가입/index.js
+++ 채륜/naver_회원가입/index.js
@@ -1,21 +1,84 @@
-const $input = document.querySelector("#pass")
-const lock1 = document.querySelector("#lock_1")
-const block_1 = document.querySelector("#PWD")
-const text = document.createElement('p')
-function PWD_CHECK(event){
-    let reg = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
-    if(reg.test(event.target.value) === false){
-        lock1.style.backgroundPosition = "25% 50%"
-        text.textContent = '8~16자, 특수문자를 1개 이상 포함하세요.'
-        text.style.fontSize = '12px'
-        text.style.textAlign = 'left'
-        text.style.marginLeft = '8px'
-        text.style.color = 'red'
-        block_1.appendChild(text)
-    } else {
-        text.remove()
-        lock1.style.backgroundPosition = "75% 0%"
+  //공용 오류정보 표시
+const join = document.querySelector(".join_row")    //공용 div
+const $id = document.querySelector("#id") //아이디
+
+const $input_1 = document.querySelector("#pass")    //비밀번호
+const lock1 = document.querySelector("#lock_1") //자물쇠 이미지
+const block_1 = document.querySelector("#PWD")  //비밀번호 칸을 감싼 div
+const $input_2 = document.querySelector('#pass_check')  //비밀번호 확인칸
+const lock2 = document.querySelector('#lock_2') //자물쇠 이미지
+const block_2 = document.querySelector('#PWD_CHECK')    //비밀번호 확인칸을 감싼 div
+
+const $name = document.querySelector('#name')   //이름
+
+function ID(event){
+    const error = document.querySelector(".error_next_box")
+    if(!$id.value){
         
+        error.textContent = '필수 정보입니다.'
+        error.style.fontSize = '12px'
+        error.style.textAlign = 'left'
+        error.style.marginTop = '8px'
+        error.style.color = 'red'
+        join.appendChild(error)
     }
 }
-$input.addEventListener("change", PWD_CHECK)
+function NAME(event){
+    const error = document.querySelector(".error_next_box")
+    if(!$name.value){
+        error.textContent = '필수 정보입니다.'
+        error.style.fontSize = '12px'
+        error.style.textAlign = 'left'
+        error.style.marginTop = '8px'
+        error.style.color = 'red'
+        join.appendChild(error)
+    }
+}
+function PWD(event){    //비밀번호 입력
+    const error = document.querySelector("#PWD .error_next_box")
+    let reg = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+    if(!$input_1.value){    //공란 시 작동
+        
+        lock1.style.backgroundPosition = "25% 50%"
+        error.textContent = '필수 정보입니다.'
+        error.style.fontSize = '12px'
+        error.style.textAlign = 'left'
+        error.style.marginTop = '8px'
+        error.style.color = 'red'
+        block_1.appendChild(error)
+    } 
+    else if(reg.test(event.target.value) === false){    //조건 불일치 시 작동
+        lock1.style.backgroundPosition = "25% 50%"
+        error.textContent = '8~16자, 특수문자를 1개 이상 포함하세요.'
+        error.style.fontSize = '12px'
+        error.style.textAlign = 'left'
+        error.style.marginTop = '8px'
+        error.style.color = 'red'
+        block_1.appendChild(error)
+    }
+    else {  //맞으면 작동
+        error.remove()
+        lock1.style.backgroundPosition = "80% 0%"
+    }
+}
+
+function CHECK(event){  //비밀번호 재확인
+    const error = document.querySelector("#PASS_CHECK .error_next_box")
+    if($input_1.value == $input_2.value){   //값이 일치하면
+        text2.remove()
+        lock2.style.backgroundPosition = "80% 0%"
+    }
+    else {  //아니면
+        lock2.style.backgroundPosition = "25% 50%"
+        text2.textContent = '비밀번호가 일치하지 않습니다.'
+        text2.style.fontSize = '12px'
+        text2.style.textAlign = 'left'
+        text2.style.marginLeft = '8px'
+        text2.style.color = 'red'
+        block_2.appendChild(text2)
+    }
+}
+$input_1.addEventListener("blur", PWD)  //blur: 다른곳으로 이동했을때
+$input_2.addEventListener("change", CHECK)  //변화시
+$id.addEventListener("blur", ID)    
+$name.addEventListener("blur", NAME)
